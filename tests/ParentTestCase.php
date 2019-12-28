9f8325af405849e0334947280be6980420e94059
Madan Sapkota <code@madansapkota.com>
2019-12-28 22:36:24 +0545

    create basic configs for phpunit test.

--- tests/ParentTestCase.php
+++ tests/ParentTestCase.php
@@ -2,9 +2,37 @@
 
 namespace Tests;
 
+use Cixware\Esewa\Client;
+use Cixware\Esewa\Exception\EsewaException;
 use PHPUnit\Framework\TestCase;
+
+// disable errors
+error_reporting(-1);
+// default timezone
+date_default_timezone_set('UTC');
 
 class ParentTestCase extends TestCase
 {
-    protected $baseUrl = 'http://localhost:8090/';
+    /**
+     * @var string $baseUrl
+     */
+    private $baseUrl = 'http://localhost:8090/';
+
+    /**
+     * @var Client $client
+     */
+    protected $client;
+
+    public function __construct()
+    {
+        try {
+            $this->client = new Client([
+                'success_url' => $this->baseUrl . 'success.php',
+                'failure_url' => $this->baseUrl . 'failed.php',
+            ]);
+        } catch (EsewaException $e) {
+            exit($e->getCode() . ' -> ' . $e->getMessage());
+        }
+        parent::__construct();
+    }
 }
