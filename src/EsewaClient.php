ae954bdb56c01e824e5c6780de96bcb47ea38d57
Madan Sapkota <code@madansapkota.com>
2019-12-28 20:03:54 +0545

    create client and load default configs.

---
+++ src/EsewaClient.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Cixware\Esewa;
+
+use Cixware\Esewa\Exception\EsewaException;
+use Cixware\Esewa\Helpers\Configure;
+use Cixware\Esewa\Payment\Payment;
+use Dotenv\Dotenv;
+
+final class EsewaClient
+{
+    use Configure;
+
+    /**
+     * @var Payment $payment
+     */
+    public $payment;
+
+    /**
+     * @param array $configs
+     * @throws EsewaException
+     */
+    public function __construct(array $configs)
+    {
+        // root directory for package
+        $rootPath = str_replace('\\', '/', dirname(__DIR__ . '../')) . '/';
+
+        // load default env variables
+        $envFile = file_exists($rootPath . '.env') ? '.env' : '.env.default';
+        $dotenv = Dotenv::createImmutable($rootPath, $envFile);
+        $dotenv->load();
+
+        // init the configs
+        $this->init($configs);
+
+        // init the classes
+        $this->payment = new Payment;
+    }
+}
