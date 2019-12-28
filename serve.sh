23eb54c08683c384decf4559995a62deaa63ef1c
Madan Sapkota <code@madansapkota.com>
2019-12-28 23:05:56 +0545

    update serve command.

--- serve.sh
+++ serve.sh
@@ -1,1 +1,17 @@
+GIT_FILE=./init.sh
+ENV_FILE=./.env
+PHPUNIT_FILE=phpunit.xml
+
+if [ ! -f "$GIT_FILE" ]; then
+    cp ./.init.default.sh ./.init.sh
+fi
+
+if [ ! -f "$ENV_FILE" ]; then
+    cp ./.env.default ./.env
+fi
+
+if [ ! -f "$PHPUNIT_FILE" ]; then
+    cp ./phpunit.default.xml ./phpunit.xml
+fi
+
 php -S localhost:8090
