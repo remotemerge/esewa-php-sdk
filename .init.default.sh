cb9682d4eca66602cb992f4193803b6baaa83073
Madan Sapkota <code@madansapkota.com>
2019-12-28 10:55:53 +0545

    create git config file.

---
+++ .init.default.sh
@@ -0,0 +1,10 @@
+# copy the file as ".init.sh" and execute
+
+# set your name, John Doe
+git config user.name ""
+
+# set your email, john@example.com
+git config user.email ""
+
+# force LF
+git config core.eol lf
