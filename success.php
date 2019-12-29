ba59985180667e227855338dd8e368fd01b60f53
Madan Sapkota <code@madansapkota.com>
2019-12-29 13:50:52 +0545

    test verification in success response.

--- success.php
+++ success.php
@@ -1,2 +1,39 @@
 <?php
-print_r($_REQUEST);
+// include the composer autoloader
+require __DIR__ . './vendor/autoload.php';
+
+use Cixware\Esewa\Client;
+
+$baseUrl = 'http://localhost:8090/';
+
+$esewa = new Client([
+    'success_url' => $baseUrl . 'success.php',
+    'failure_url' => $baseUrl . 'failed.php',
+]);
+
+// placeholder fields
+$productId = $referenceId = $amount = null;
+
+// check reference field
+if (isset($_GET['refId'])) {
+    $referenceId = $_GET['refId'];
+}
+
+// check product field
+if (isset($_GET['oid'])) {
+    $productId = $_GET['oid'];
+}
+
+// check amount field
+if (isset($_GET['amt'])) {
+    $amount = $_GET['amt'];
+}
+
+if ($referenceId !== null || $productId !== null || $amount !== null) {
+    $status = $esewa->payment->verify($referenceId, $productId, $amount);
+    if (isset($status->verified) && $status->verified) {
+        print_r($status);
+        exit('<h1>The payment is verified.</h1>');
+    }
+}
+exit('<h1>The payment is not verified.</h1>');
