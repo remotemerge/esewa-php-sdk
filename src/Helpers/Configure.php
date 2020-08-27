127ff24b1dacb99a0ff8540a3f3b5c66c0bed499
Madan Sapkota <code@madansapkota.com>
2020-08-27 23:20:02 +0545

    Update config loader

--- src/Helpers/Configure.php
+++ src/Helpers/Configure.php
@@ -2,47 +2,62 @@
 
 namespace Cixware\Esewa\Helpers;
 
-use Cixware\Esewa\Exception\EsewaException;
-
 trait Configure
 {
     /**
      * @var bool $isProduction
      */
-    private $isProduction = false;
+    protected static $isProduction = false;
+
+    /**
+     * @var string $baseUrl
+     */
+    protected static $baseUrl = 'https://uat.esewa.com.np';
+
+    /**
+     * @var string $successUrl
+     */
+    protected static $successUrl;
+
+    /**
+     * @var string $failureUrl
+     */
+    protected static $failureUrl;
+
+    /**
+     * @var string $merchantCode
+     */
+    protected static $merchantCode = 'epay_payment';
 
     /**
      * @param array $configs
-     * @throws EsewaException
      */
     private function init(array $configs): void
     {
         // set app environment
-        if (isset($configs['is_production']) && $configs['is_production']) {
-            $this->isProduction = true;
-            putenv('ESEWA_IS_PRODUCTION=true');
+        if (isset($configs['is_production']) && is_bool($configs['is_production'])) {
+            self::$isProduction = $configs['is_production'];
         }
 
         // set success url
         if (isset($configs['success_url']) && filter_var($configs['success_url'], FILTER_VALIDATE_URL)) {
-            putenv('ESEWA_SUCCESS_URL=' . $configs['success_url']);
+            self::$successUrl = $configs['success_url'];
         }
 
         // set failure url
         if (isset($configs['failure_url']) && filter_var($configs['failure_url'], FILTER_VALIDATE_URL)) {
-            putenv('ESEWA_FAILURE_URL=' . $configs['failure_url']);
+            self::$failureUrl = $configs['failure_url'];
         }
 
         // production mode
-        if ($this->isProduction) {
-            // set base URL
-            putenv('ESEWA_BASE_URL=' . getenv('ESEWA_PRODUCTION_URL'));
+        if (self::$isProduction) {
+            // reset base URL
+            self::$baseUrl = 'https://esewa.com.np';
 
             // set merchant code
             if (!isset($configs['merchant_code']) || empty($configs['merchant_code'])) {
-                throw new EsewaException('The merchant_code field is required in production.');
+                self::$merchantCode = $configs['merchant_code'];
             }
-            putenv('ESEWA_MERCHANT_CODE=' . $configs['merchant_code']);
         }
     }
 }
