cd80b07ccaa7c1182c975e92bf1a657dc3cd87d8
Madan Sapkota <code@madansapkota.com>
2020-08-27 19:19:51 +0545

    Use setUp method

--- tests/ParentTestCase.php
+++ tests/ParentTestCase.php
@@ -6,34 +6,32 @@ use Cixware\Esewa\Client;
 use Cixware\Esewa\Exception\EsewaException;
 use PHPUnit\Framework\TestCase;
 
-// disable errors
-error_reporting(-1);
-
-// default timezone
-date_default_timezone_set('UTC');
-
 class ParentTestCase extends TestCase
 {
     /**
      * @var string $baseUrl
      */
-    private $baseUrl = 'http://localhost:8090/demo/';
+    private static $baseUrl = 'http://localhost:8090/demo/';
 
     /**
      * @var Client $client
      */
     protected $client;
 
-    public function __construct()
+    /**
+     * @throws EsewaException
+     */
+    protected function setUp(): void
     {
-        try {
-            $this->client = new Client([
-                'success_url' => $this->baseUrl . 'success.php',
-                'failure_url' => $this->baseUrl . 'failed.php',
-            ]);
-        } catch (EsewaException $e) {
-            exit($e->getCode() . ' -> ' . $e->getMessage());
-        }
-        parent::__construct();
+        parent::setUp();
+
+        // default timezone
+        date_default_timezone_set('UTC');
+
+        // init client
+        $this->client = new Client([
+            'success_url' => self::$baseUrl . 'success.php',
+            'failure_url' => self::$baseUrl . 'failed.php',
+        ]);
     }
 }
