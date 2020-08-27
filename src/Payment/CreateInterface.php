08f2a1316f0f64e9616cafa969e491c1925a71e8
Madan Sapkota <code@madansapkota.com>
2020-08-28 00:04:16 +0545

    Update method name

--- src/Payment/CreateInterface.php
+++ src/Payment/CreateInterface.php
@@ -5,5 +5,5 @@ namespace Cixware\Esewa\Payment;
 interface CreateInterface
 {
     // request for payment
-    public function create(string $productId, float $amount, float $taxAmount, float $serviceAmount = 0, float $deliveryAmount = 0): void;
+    public function process(string $productId, float $amount, float $taxAmount, float $serviceAmount = 0, float $deliveryAmount = 0): void;
 }
