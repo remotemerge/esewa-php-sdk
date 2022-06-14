7a53bfa0232b7640b965d1dbe5b89ef985b2adb3
Madan Sapkota <contact@madansapkota.com>
2022-06-14 15:21:25 +0545

    Create CS Fixer config

---
+++ .php-cs-fixer.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+// directories and files
+$finder = PhpCsFixer\Finder::create()
+    ->in([
+        __DIR__ . '/demo',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->name('php');
+
+// init config
+$config = new PhpCsFixer\Config();
+$config->setUsingCache(false)
+    ->setRiskyAllowed(true);
+
+// set rules
+return $config->setRules([
+    '@PSR1' => true,
+    '@PSR12:risky' => true,
+    '@PHP74Migration:risky' => true,
+    // custom
+    'single_quote' => true,
+])->setFinder($finder);
