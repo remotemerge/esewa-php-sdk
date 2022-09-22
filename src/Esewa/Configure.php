b590d9691b8ab0b0a2d20e188ff8e584a6914f8a
Madan Sapkota <contact@madansapkota.com>
2022-09-22 15:42:26 +0545

    Create eSewa configuration class

---
+++ src/Esewa/Configure.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace Cixware\Payment\Esewa;
+
+final class Configure
+{
+    /**
+     * API url for development
+     */
+    public string $apiUrl = 'https://uat.esewa.com.np';
+
+    /**
+     * The merchant code provided by eSewa
+     */
+    public string $merchantCode;
+
+    /**
+     * The callback url for successful transaction
+     */
+    public string $successUrl;
+
+    /**
+     * The callback url for failed transaction
+     */
+    public string $failureUrl;
+
+    public function __construct(string $successUrl, string $failureUrl, string $merchantCode = 'EPAYTEST', string $environment = 'development')
+    {
+        $this->successUrl = $successUrl;
+        $this->failureUrl = $failureUrl;
+        $this->merchantCode = $merchantCode;
+
+        // set API url for production
+        if ($environment === 'production') {
+            $this->apiUrl = 'https://esewa.com.np';
+        }
+    }
+}
