8b15f54302aa99692a1c083f0839b7a5641714a9
Madan Sapkota <contact@madansapkota.com>
2022-09-22 16:47:44 +0545

    Update demo process code

--- demo/process.php
+++ demo/process.php
@@ -1,5 +1,8 @@
 <?php declare(strict_types=1);
 
+use Cixware\Esewa\Client;
+use Cixware\Esewa\Config;
+
 // init autoloader
 require dirname(__DIR__) . '/vendor/autoload.php';
 
@@ -7,15 +10,14 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 $successUrl = 'http://localhost:8090/demo/success.php';
 $failureUrl = 'http://localhost:8090/demo/failed.php';
 
-$config = new \Cixware\Esewa\Config($successUrl, $failureUrl);
+$config = new Config($successUrl, $failureUrl);
 
 try {
-    $esewa = new \Cixware\Esewa\Client($config);
+    $esewa = new Client($config);
 
     // generate random product ID
     $productId = hash('SHA256', bin2hex(random_bytes(8)));
     $esewa->process(substr($productId, 0, 16), 100, 10);
-
 } catch (Exception $exception) {
     exit($exception->getMessage());
 }
