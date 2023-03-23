d13f1c931a1119052b10921618200463252410c6
Madan Sapkota <contact@madansapkota.com>
2023-03-23 22:36:52 +0545

    Optimize success handler

--- demo/success.php
+++ demo/success.php
@@ -1,32 +1,29 @@
 <?php declare(strict_types=1);
 
-// init autoloader
-require dirname(__DIR__) . '/vendor/autoload.php';
-
 use Cixware\Esewa\Client;
 use Cixware\Esewa\Config;
 use GuzzleHttp\Exception\GuzzleException;
 
-// format params
-$demoUrl = 'http://localhost:8090/demo/';
-$successUrl = $demoUrl . 'success.php';
-$failureUrl = $demoUrl . 'failed.php';
+// Require the autoloader
+require dirname(__DIR__) . '/vendor/autoload.php';
 
+// Set up the configuration object
+$successUrl = 'http://localhost:8090/demo/success.php';
+$failureUrl = 'http://localhost:8090/demo/failed.php';
 $config = new Config($successUrl, $failureUrl);
+
+// Initialize the client
 $esewa = new Client($config);
 
-// placeholder fields
+// Get the query parameters
 $productId = $_GET['oid'] ?? null;
 $referenceId = $_GET['refId'] ?? null;
 $amount = $_GET['amt'] ?? null;
 
 try {
+    // Verify the payment and output the result
     $status = $esewa->verify($referenceId, $productId, (float)$amount);
-    if ($status) {
-        exit('The payment is verified.');
-    }
-
-    exit('The payment is not verified.');
+    exit($status ? 'The payment is verified.' : 'The payment is not verified.');
 } catch (GuzzleException|JsonException $e) {
     exit($e->getMessage());
 }
