f2fa85f1adfa18671f141536df748d552032abee
Madan Sapkota <contact@madansapkota.com>
2024-08-24 17:48:50 +0545

    Update API on demo files

--- demo/success.php
+++ demo/success.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use RemoteMerge\Esewa\Client;
-use RemoteMerge\Esewa\Config;
 
 // Require the autoloader
 require dirname(__DIR__) . '/vendor/autoload.php';
@@ -11,10 +10,13 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 // Set up the configuration object
 $successUrl = 'http://localhost:8090/demo/success.php';
 $failureUrl = 'http://localhost:8090/demo/failed.php';
-$config = new Config($successUrl, $failureUrl);
 
 // Initialize the client
-$esewa = new Client($config);
+$esewa = new Client([
+    'merchant_code' => 'EPAYTEST',
+    'success_url' => $successUrl,
+    'failure_url' => $failureUrl,
+]);
 
 // Get the query parameters
 $productId = $_GET['oid'] ?? null;
@@ -23,7 +25,7 @@ $amount = $_GET['amt'] ?? null;
 
 try {
     // Verify the payment and output the result
-    $status = $esewa->verify($referenceId, $productId, (float) $amount);
+    $status = $esewa->verifyPayment($referenceId, $productId, (float) $amount);
     exit($status ? 'The payment is verified.' : 'The payment is not verified.');
 } catch (Exception $exception) {
     exit($exception->getMessage());
