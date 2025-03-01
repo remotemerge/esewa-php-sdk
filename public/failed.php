7d4ba6ab6103d2e35eef92d209b130c486462c26
Madan Sapkota <contact@madansapkota.com>
2025-03-01 12:11:40 +0000

    Print response

--- public/failed.php
+++ public/failed.php
@@ -5,4 +5,8 @@ declare(strict_types=1);
 // init autoloader
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-exit(print_r($_REQUEST, true));
+// Print response
+$params = $_REQUEST;
+foreach ($params as $k => $v) {
+    printf("%s: %s\n", $k, $v);
+}
