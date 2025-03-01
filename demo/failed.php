fa7cd230fcc993bfef45cfc0cf1cda29d1309e81
Madan Sapkota <contact@madansapkota.com>
2025-03-01 11:25:54 +0000

    Use require_once for autoloader

--- demo/failed.php
+++ demo/failed.php
@@ -3,6 +3,6 @@
 declare(strict_types=1);
 
 // init autoloader
-require dirname(__DIR__) . '/vendor/autoload.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 exit(print_r($_REQUEST, true));
